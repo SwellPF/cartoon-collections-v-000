d1dfd8662481c5568ece2d8f0c4def53db5955a0
Paul Fell <swellpf@gmail.com>
2018-09-25 19:33:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    snacks.each do |snack|
+    snacks.find do |snack|
     if cheese_types.include?(snack)
       return snack
     end
