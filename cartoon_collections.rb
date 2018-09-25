9019c9bb87836453138acb5a849c553e7559e014
Paul Fell <swellpf@gmail.com>
2018-09-25 19:46:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    snacks.find { |snack| snacks.include?(snack) ? snack : nil
+    snacks.find { |snack| snacks.include?(snack) ? snack : nil}
 end
